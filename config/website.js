9cf72e8e106345bfe178518b2cb4738238a6742c
Andrew Mason <andrew.mason@n2pub.com>
2018-10-10 20:17:36 -0400

    first commit

---
+++ config/website.js
@@ -0,0 +1,21 @@
+const tailwind = require('../tailwind');
+
+module.exports = {
+  pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "/portfolio"
+
+  siteTitle: 'Gatsby Starter Portfolio', // Navigation and Site Title
+  siteTitleAlt: '', // Alternative Site title for SEO
+  siteUrl: '', // Domain of your site. No trailing slash!
+  siteLanguage: 'en', // Language Tag on <html> element
+  siteLogo: '/logos/logo-1024.png', // Used for SEO and manifest
+  siteDescription: '',
+
+  // siteFBAppID: '123456789', // Facebook App ID - Optional
+  userTwitter: '', // Twitter Username
+  ogSiteName: '', // Facebook Site Name
+  ogLanguage: 'en_US', // Facebook Language
+
+  // Manifest and Progress color
+  themeColor: tailwind.colors.orange,
+  backgroundColor: tailwind.colors.blue,
+};
